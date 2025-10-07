d114cb1acea16d4ca5a4393b58ab64bcba176459
ansarulSohan <ansarulsohan@gmail.com>
2025-10-07 12:26:18 +0200

    Corrects redis cache format to strings

--- proxy.js
+++ proxy.js
@@ -7,49 +7,36 @@ const cachingProxy = (port, origin) => {
   app.use(express.json());
 
   app.use(async (req, res) => {
-    const PROTOCOL = req.protocol || "https";
-    const URL = `${PROTOCOL}://${origin}${req.url}`;
+    const PROTOCOL = "https";
+    const path = req.url !== "/" ? req.url : "";
+    const URL = `${PROTOCOL}://${origin}${path}`;
     const key = `${req.method}:${URL}`;
     console.log(key);
 
-    const cache = await redisClient();
+    try {
+      const cache = await redisClient();
 
-    const cacheResponseStringified = await cache.get(key);
-    const cacheResponse = JSON.parse(cacheResponseStringified);
+      const cacheResponseStringified = await cache.get(key);
+      const cacheResponse = JSON.parse(cacheResponseStringified);
 
-    if (!cacheResponse) {
-      const { headers, data, status } = await axios({
-        url: URL,
-        method: req.method,
-        responseType: "json",
-        headers: {
-          ...req.headers,
-          host: origin,
-          "user-agent": req.headers["user-agent"] || "Mozilla/5.0",
-        },
-        data: ["POST", "PUT", "PATCH", "DELETE"].includes(req.method)
-          ? { ...req.body }
-          : undefined,
-      });
+      if (!cacheResponse) {
+        const { data, status } = await axios.get(URL);
 
-      if (status !== 200 || !data) {
-        return res.status(500).json({ msg: "Internal Server Error" });
+        if (status !== 200 || !data) {
+          return res.status(500).json({ msg: "Internal Server Error" });
+        }
+        await cache.set(key, JSON.stringify({ status, data }));
+
+        return res.status(status).set("X-cache", "MISS").send(data);
       }
 
-      console.log(status);
-
-      await cache.set(key, JSON.stringify({ headers, status, data }));
-
       return res
-        .status(status)
-        .set({ ...headers, "X-cache": "MISS" })
-        .send(data);
+        .status(cacheResponse.status)
+        .set("X-cache", "HIT")
+        .send(cacheResponse.data);
+    } catch (error) {
+      return res.send(error);
     }
-
-    return res
-      .status(cacheResponse.status)
-      .set({ ...cacheResponse.headers, "X-cache": "HIT" })
-      .send(cacheResponse.data);
   });
 
   app.listen(port ?? 3000, () => {
